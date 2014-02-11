0fe4cc7c55390a7a9e2714e07b5c2134e534acfb
Erlend Hamnaberg <erlend@hamnaberg.net>
2014-02-12 00:23:37 +0100

    Do not copy files, build build zip structure in memory

--- src/main/scala/appsh/Appsh.scala
+++ src/main/scala/appsh/Appsh.scala
@@ -10,6 +10,7 @@ object Appsh extends Plugin {
     val appshZipDirectory = SettingKey[File]("directory that will be zipped. Usually target/appsh")
     val appshZipFile = SettingKey[File]("Zipfile. Usually target/appsh.zip")
     val appshBuild = TaskKey[File]("appsh-build", "Creates a app.sh zip file.")
+    val appshPermissions = TaskKey[Map[String, Int]]("appsh-permissons", "Map from path to unix permissions")
   }
 
   import AppshKeys._
@@ -18,80 +19,90 @@ object Appsh extends Plugin {
     appshSource := baseDirectory.value / "src" / "appsh",
     appshZipDirectory := target.value / "appsh",
     appshZipFile := target.value / "appsh.zip",
-    appshBuild <<= (appshSource, appshZipDirectory, appshZipFile, streams) map { (src, target, zip, stream) => 
+    appshPermissions := Map(
+      "bin/root/*" -> Integer.decode("0755"), 
+      "hooks/*" -> Integer.decode("0755")
+    ),
+    appshBuild <<= (appshSource, appshZipDirectory, appshPermissions, appshZipFile, streams) map { (src, target, permissions, zip, stream) => 
       IO.delete(zip)
-      IO.delete(target)
-      prepareZipDirectory(src, target)
-      makeZip(target, zip, stream)
+      
+      val filesystem = buildFilesystem(target, src)
+      val perms = preparePermissions(filesystem, permissions)
+
+      makeZip(filesystem, perms, zip, stream)
     }
   )
 
-  def prepareZipDirectory(src: File, target: File) {
-    val root = target / "root"
-    IO.createDirectories(Seq(target, root))
-    IO.copyDirectory(src, target, overwrite = false)
-    ()
+  def buildFilesystem(target: File, src: File) = {
+    def entries(f: File): List[File] = f :: (if (f.isDirectory) IO.listFiles(f).toList.flatMap(entries) else Nil)
+    def tupled(root: File) = (f: File) => f.getAbsolutePath.substring(root.toString.length).substring(1) -> f
+    val withoutTarget = entries(target).tail.map(tupled(target)).toMap
+    val withoutSrc = entries(src).tail.map(tupled(src)).toMap
+
+    withoutTarget ++ withoutSrc
   }
 
-  def makeZip(destDir: File, zipFile: File, stream: TaskStreams) = {    
-    validate(destDir)
-
-    def entries(f: File):List[File] = f :: (if (f.isDirectory) IO.listFiles(f).toList.flatMap(entries) else Nil)
-
-    def zip(files: Seq[File], file: File) {
-      val entries = files.map{ f =>
-        val path = f.getAbsolutePath.substring(destDir.toString.length)
-        val e = new ZipArchiveEntry(f, if (path.startsWith("/")) path.substring(1) else path)
-        if (path.contains("bin") || path.contains("hooks") && f.isFile) {
-          e.setUnixMode(Integer.decode("0755"))
-        }
-        (f, e)
+  def preparePermissions(filesystem: Map[String, File], permissions: Map[String, Int]) = {
+    permissions.flatMap { case (path, perm) => 
+      val actualKey = {
+        val noStar = if (path.endsWith("*")) path.substring(0, path.length - 1) else path
+        if (noStar.startsWith("/")) noStar.substring(1) else noStar
       }
+      filesystem.flatMap {case (path2, _) => if (path2.startsWith(actualKey)) Map(path2 -> perm) else Map.empty[String, Int]}
+    }.toMap
+  }
 
-      val os = new ZipArchiveOutputStream(file)
-      entries.foreach{case (f, e) =>
-        os.putArchiveEntry(e)
-        if (!f.isDirectory) {
-          IO.transfer(f, os)
-        }
-        os.closeArchiveEntry()
-      }
-      os.close()
+  def makeZip(fileMap: Map[String, File], permissionMap: Map[String, Int], zipFile: File, stream: TaskStreams) = {
+    validate(fileMap)
+    val containerDirectory = zipFile.getAbsoluteFile.getParentFile
+    if (!containerDirectory.exists) {
+      IO.createDirectory(containerDirectory)
     }
+
+    val entries = fileMap.map { case (path, f) =>
+      val e = new ZipArchiveEntry(f, path)
+      permissionMap.get(path).foreach(p => e.setUnixMode(p))
+      (f, e)
+    }
+
+    val os = new ZipArchiveOutputStream(zipFile)
     
-    zip(entries(destDir).tail, zipFile)
+    entries.foreach{case (f, e) =>
+      os.putArchiveEntry(e)
+      if (f.isFile) {
+        IO.transfer(f, os)
+      }
+      os.closeArchiveEntry()
+    }
+    os.close()
+
     stream.log.info("Wrote " + zipFile)
     zipFile
   }
 
-  def validate(appshDir: File) = {
-    val config = appshDir / "app.config"
-    val root = appshDir / "root"
-    val bin = root / "bin"
-    val hooks = appshDir / "hooks"
+  def negate[A](p: (A) => Boolean) = (a: A) => !p(a)
+
+  def validate(fileMap: Map[String, File]) {
+    val config = fileMap.get("app.config")
+    val bin = fileMap.get("root/bin")
+    val postInstall = fileMap.get("hooks/post-install")
     
-    if (!config.exists && !config.isFile) {
+    val fileExists = (c: File) => c.exists && c.isFile
+    val dirExists = (c: File) => c.exists && c.isDirectory
+
+    if (!config.forall(fileExists)) {
       sys.error("Missing app.config file")
     }
 
-    if (!root.exists && !root.isDirectory) {
-      sys.error("Not a valid apps.sh directory, please make sure there is a 'root' directory.")
-    }
-    if (hooks.exists) {
-      val postInstall = hooks / "post-install"
-      if (!postInstall.exists && !postInstall.isFile) {
-        sys.error(s"$postInstall does not exist or is not a file")
+    if (!bin.forall(dirExists)) {
+      sys.error("Not a valid apps.sh directory, please make sure there is a 'root/bin' directory.")
+      if (!fileMap.exists{case (n, f) => n.startsWith("root/bin/") && fileExists(f) }) {
+        sys.error("No executables found in root/bin")
       }
     }
-    if (bin.exists && bin.isDirectory) {
-      val files = bin.listFiles()
-      if (files == null || files.isEmpty) {
-       sys.error("There are no start scripts for your app")
-      }
-    }
-    else {
-      sys.error("Not a valid apps.sh directory, please make sure there is a 'bin' directory in 'root'.")
+
+    if (!postInstall.forall(negate(fileExists))) {
+        sys.error("'hooks/post-install' does not exist or is not a file")
     }
   }
-
 }
